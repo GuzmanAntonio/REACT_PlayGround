55d49561f92e9fcfb0c4853f7d0d61f9c2e2007a
Antonio Guzman <antonioguzman@Antonios-MacBook-Air.local>
2017-11-02 23:44:05 -0400

    REACT: Search App & ChatterApp

--- src/ChatterApp/ChatterApp.js
+++ src/ChatterApp/ChatterApp.js
@@ -1,19 +1,38 @@
 import React, {Component} from 'react'
-import DATA from './DATA'
 import './styles.css'
 import faker from 'faker'
 import Chats from './Chat'
 
 class ChatterApp extends Component {
   state = {
-    chats: undefined
+    chats: undefined,
+    message: undefined,
+    username: undefined,
   }
-  componentDidMount () {
+  componentDidMount () { //WHEN THIS PAGE LOADS
     alert('COMPONENT DID MOUNT')
 
     setTimeout(() => {
       this.setChatData()
     }, 1000)
+  }
+
+  setAvatar = (e) => {
+    let avatar = e.target.value
+    this.setState({avatar: avatar})
+    console.log(this.state.avatar)
+  }
+
+  setUserName = (e) => {
+    let username = e.target.value
+    this.setState({username: username})
+    console.log(this.state.username)
+  }
+
+  setMessage = (e) => {
+    let message = e.target.value
+    this.setState({message: message})
+    console.log(this.state.message)
   }
 
   setChatData () {
@@ -29,6 +48,20 @@ class ChatterApp extends Component {
     }
     this.setState({chats: chats})
   }
+  
+  submitInfo = (e) => {
+    e.preventDefault()
+    let newChat = {
+      avatar: this.state.avatar,
+      username: this.state.username,
+      message: this.state.message
+    }
+    let allChats = this.state.chats
+    allChats.unshift(newChat)
+    this.setState({chats: allChats})
+
+  }
+
   render () {
     return (
       <div>
@@ -36,6 +69,12 @@ class ChatterApp extends Component {
         {
           this.state.chats ? <Chats chats={this.state.chats} /> : 'Loading...'
         }
+        <form>
+          <input placeholder='Message' onChange={this.setMessage}/>
+          <input placeholder='Username'onChange={this.setUserName}/>
+          <input placeholder='Avatar' onChange={this.setAvatar}/>
+          <button type='submit' onClick={this.submitInfo}>Submit</button>
+        </form>
       </div>
     )
   }
